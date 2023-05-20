e23d090180661cf3bbb3bc9973346f2a354d1e25
Bradley Newman <bnewman227@gmail.com>
2023-05-20 18:38:16 +0930

    Add files

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# Food Decision Making Machine
+
+## Usage
+
+create a `.env` file with the following environment variables:
+
+`PORT=<portnumber>`
+`CHOICES_URL = <url-to-choices-txt>`
+`CHOICES_WOOLIES_URL = <url-to-woolies-choices-txt>`
+
+`npm i` then run with `npm run start`
---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { choose, fetchFileContents } from './utils';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (_, res) => {
+  res.send('Food Decision Making Machine Server - try the \'/choose\' different route');
+});
+
+app.get('/choose', async (_, res) => {
+  try {
+    let choices: string = await fetchFileContents(process.env.CHOICES_URL!);
+    let choicesWoolies: string = await fetchFileContents(process.env.CHOICES_WOOLIES_URL!);
+    // Join the standard choices with woolies as either should be available
+    choices = choices.concat(choicesWoolies);
+    const standardChoice: string = choose(choices);
+    const wooliesChoice: string = choose(choicesWoolies);
+    res.send({ choices: { standard: standardChoice, woolies: wooliesChoice } });
+  } catch (error) {
+    res.send({ Error: error })
+  }
+});
+
+app.listen(port, () => {
+  console.log(`food-decision-making-machine: Server is running at http://localhost:${port}`);
+});
