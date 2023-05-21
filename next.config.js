ce8cca18e95d9bdee2daa3a683bcc93dd7f09fb7
Bradley Newman <bnewman227@gmail.com>
2023-05-21 11:13:41 +0930

    Add API endpoint to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        REACT_APP_FDMM_ROUTE: "http://heapsgoodservers.com:1338/choose"
+    }
+}
 
 module.exports = nextConfig
