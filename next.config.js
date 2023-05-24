4f188c1d9c49fc9541fb82c0f8b6eaeb3deb499f
Bradley Newman <bnewman@daronmont.com.au>
2023-05-24 12:47:01 +0930

    Update way of dealing with taglines to make it easy to add more

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     env: {
         REACT_APP_SERVERLESS: "true",
         REACT_APP_CHOICES_URL: "https://raw.githubusercontent.com/soda3x/food-decision-making-machine/main/choices.txt",
-        REACT_APP_CHOICES_WOOLIES_URL: "https://raw.githubusercontent.com/soda3x/food-decision-making-machine/main/choices-woolies.txt"
+        REACT_APP_CHOICES_WOOLIES_URL: "https://raw.githubusercontent.com/soda3x/food-decision-making-machine/main/choices-woolies.txt",
+        REACT_APP_TAGLINES_URL: "https://raw.githubusercontent.com/soda3x/food-decision-making-machine/main/taglines.txt"
     }
 }
 
--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -1,38 +1,13 @@
 import { choose, fetchFileContents } from "choose-for-me";
 
-export function getTagline(): string {
+export async function getTagline(): Promise<string> {
     const day = getHumorousDayName()
-    const taglines: string[] = [
-        `Happy ${day}!`, `Wow, as if it's already ${day}`,
-        `Hooray for ${day}!`, `What are we eating on this fine ${day}?`,
-        `The Rat Dog better not have brought lunch on this historic ${day}...`,
-        `>python food.py -w`,
-        `Are the towels on the Titanic wet?`,
-        `NAAUUURGGHHH!`,
-        `better that the original`,
-        `AMP CHAFF!`,
-        `instead of having dessert, just have more main`,
-        `tony said he'd drive`,
-        `Drinking game: take a shot every time Matt runs into someone he knows`,
-        `If Matt's upstairs, we're not waiting for him`,
-        `And to wash this down, how about a nice ALC?`,
-        `Chris Mathie`,
-        `Tim Allen`,
-        `Conduit pods`,
-        `Bitbucket proxy is down again, brad run the macro`,
-        `I won't be mad, just give me my jacket back!`,
-        `Why would someone STEAL a jacket?!`,
-        `s**t is the opposite of taste`,
-        `mum said its my turn to go on a costco run with rhys`,
-        `keep in touch with yourself`,
-        `s**t pie`,
-        `haydens social club IOU is larger than Einstein's IQ`
-    ]
-    const randomIndex = Math.floor(Math.random() * taglines.length);
+    const taglines = await fetchFileContents(process.env.REACT_APP_TAGLINES_URL!);
+    const tagline = choose(taglines, '\n');
     if (day == 'Hope you\'re not working today...') {
         return day;
     } else {
-        return taglines[randomIndex];
+        return tagline.replace("%DAY%", day);
     }
 }
 
